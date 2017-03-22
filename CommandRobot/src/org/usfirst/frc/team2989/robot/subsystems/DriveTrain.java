6c366a130aaf60de80f53638cab1c43451344406
Matt Yokan <matt@yokanovich.org>
2017-03-22 07:41:31 -0500

    Altered Mecanum Drivetrain
    Modifies Mecanum Drivetrain to work properly - removed field orientated method and replaced it with method 
    
    **This may require further modification to make the control scheme better - once we revert back to joystick based control instead of xbox controller control (due to accuracy), I may have to change the OI to make the control intuitive**

--- CommandRobot/src/org/usfirst/frc/team2989/robot/subsystems/DriveTrain.java
+++ CommandRobot/src/org/usfirst/frc/team2989/robot/subsystems/DriveTrain.java
@@ -20,7 +20,7 @@ public class DriveTrain extends Subsystem {
 		setDefaultCommand(new TeleopDriveCommand());
 	}
 	
-	public void driveRobot(double x, double y, double rotation, double angle) {
-		robotDrive.mecanumDrive_Cartesian(x, y, rotation, angle);
+	public void driveRobot(double magnitude, double direction, double rotation) {
+		robotDrive.mecanumDrive_Cartesian(magnitude, direction, rotation);
 	}
 }
